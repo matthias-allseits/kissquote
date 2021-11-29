63331f8a87238ee4cad5e14ca4b64343860c58a2
Matthias Allseits <matthias.allseits@exxas.net>
2021-11-29 20:07:53 +0100

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {LandingpageComponent} from './sites/landingpage/landingpage.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/landingpage', pathMatch: 'full' },
+  { path: 'landingpage', component: LandingpageComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
