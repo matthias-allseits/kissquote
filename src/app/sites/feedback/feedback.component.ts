7dd2e901651287268166d99de119ce97d3bb9e74
Matthias Allseits <matthias.allseits@exxas.net>
2021-12-20 18:27:32 +0100

    frontend addings and adjustments

--- src/app/sites/feedback/feedback.component.ts
+++ src/app/sites/feedback/feedback.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {TranslationService} from '../../services/translation.service';
 
 @Component({
     selector: 'app-feedback',
@@ -9,9 +10,11 @@ export class FeedbackComponent implements OnInit {
 
     public activeMood: string;
     public placeholder = '';
+    public submitText = '';
 
-    constructor() {
-    }
+    constructor(
+        public tranService: TranslationService
+    ) { }
 
     ngOnInit(): void {
         this.changeMood('bad');
@@ -24,12 +27,15 @@ export class FeedbackComponent implements OnInit {
         switch (mood) {
             case 'bad':
                 this.placeholder = 'Der Programmator dieser bunten Webseiten möge an seinem Kot ersticken!';
+                this.submitText = 'Take that!';
                 break;
             case 'medium':
                 this.placeholder = 'Ist ja ganz nett hier, aber ich fühle mich in meiner Meinung bestärkt, dass künftig für die Erstellung neuer bunter Webseiten ein Baugesuch gestellt werden sollte.';
+                this.submitText = 'Submit';
                 break;
             case 'good':
                 this.placeholder = 'Wir werden unseren Erstgeborenen ' + 'Kissquote' + ' nennen.';
+                this.submitText = 'Send my kiss';
                 break;
         }
     }
