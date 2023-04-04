956ebb927ddcd8255c5f5ff45114dc514bb439d0
Matthias Allseits <java@allseits.ch>
2023-04-04 21:28:24 +0200

    shareinfo-charts adjustments

--- src/app/models/sharehead-share.ts
+++ src/app/models/sharehead-share.ts
@@ -473,7 +473,7 @@ export class ShareheadShare {
                     backgroundColor: 'rgb(220, 57, 18, 1)',
                     hoverBackgroundColor: 'rgb(220, 57, 18, 0.5)',
                     pointBackgroundColor: 'rgb(220, 57, 18, 1)',
-                    pointBorderColor: 'rgb(220, 57, 18, 1)',
+                    pointBorderColor: 'rgb(220, 57, 18, 0)',
                     pointHoverBackgroundColor: 'rgba(220, 57, 18, 1)',
                     pointHoverBorderColor: 'rgba(220, 57, 18, 1)',
                 },
@@ -521,7 +521,6 @@ export class ShareheadShare {
                     backgroundColor: 'rgb(220, 57, 18, 1)',
                     hoverBackgroundColor: 'rgb(220, 57, 18, 0.5)',
                     pointBackgroundColor: 'rgb(220, 57, 18, 1)',
-                    pointBorderColor: 'rgb(220, 57, 18, 1)',
                     pointHoverBackgroundColor: 'rgba(220, 57, 18, 1)',
                     pointHoverBorderColor: 'rgba(220, 57, 18, 1)',
                 }
@@ -529,7 +528,7 @@ export class ShareheadShare {
         };
 
         this.yahooBalances()?.forEach(balance => {
-            if (balance.ordinarySharesNumber && balance.treasurySharesNumber) {
+            if (balance.ordinarySharesNumber !== undefined && balance.treasurySharesNumber !== undefined) {
                 chartData.labels?.push(balance.year);
                 chartData.datasets[0].data.push(balance.ordinarySharesNumber);
                 chartData.datasets[1].data.push(balance.treasurySharesNumber);
