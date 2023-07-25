8f8d9792d791a9a8ccdb32d7eca99c8bb8eb13f7
Matthias Allseits <java@allseits.ch>
2023-07-25 20:26:10 +0200

    dashboard-balances-tab refactoring

--- src/app/components/dashboard-balance/dashboard-balance.component.ts
+++ src/app/components/dashboard-balance/dashboard-balance.component.ts
@@ -1,21 +1,30 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {TranslationService} from "../../services/translation.service";
 import {DividendTotals, Portfolio} from "../../models/portfolio";
 import {ChartData} from "chart.js";
 import {BankAccount} from "../../models/bank-account";
+
+export interface AccountBalance {
+    account: BankAccount;
+    chartData: ChartData;
+}
 
 @Component({
   selector: 'app-dashboard-balance',
   templateUrl: './dashboard-balance.component.html',
   styleUrls: ['./dashboard-balance.component.scss']
 })
-export class DashboardBalanceComponent implements OnInit {
+export class DashboardBalanceComponent implements OnInit, OnChanges {
 
     @Input() portfolio?: Portfolio;
     @Input() dividendLists?: DividendTotals[];
 
+    public dividendIncomeChartData?: ChartData;
     public incomeChartDataImproved?: ChartData;
+    public yieldChartData?: ChartData;
+    public investmentChartData?: ChartData;
     public incomeChartDataImprovedBoxHeight = 143;
+    public accountBalances: AccountBalance[] = [];
 
     constructor(
         public tranService: TranslationService,
@@ -27,6 +36,20 @@ export class DashboardBalanceComponent implements OnInit {
             this.incomeChartDataImprovedBoxHeight = 300;
         }
         this.incomeChartDataImproved = this.portfolio?.incomeChartDataImproved();
+        if (this.portfolio) {
+            this.portfolio.bankAccounts.forEach(account => {
+                const data = this.getBalanceChartDataByAccount(account);
+                this.accountBalances.push(
+                    { account: account, chartData: data }
+                );
+            });
+            this.investmentChartData = this.portfolio.investmentChartData();
+        }
+    }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        this.dividendIncomeChartData = this.portfolio?.dividendIncomeChartData();
+        this.yieldChartData = this.portfolio?.yieldChartData();
     }
 
     getBalanceChartDataByAccount(account: BankAccount): ChartData {
