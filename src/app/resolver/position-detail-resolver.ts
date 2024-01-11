a1f4e4f4f9d8c4be72556af21e711e1fe0e0ceb0
Matthias Allseits <java@allseits.ch>
2024-01-11 12:56:04 +0100

    newest rate add to chart bugfix

--- src/app/resolver/position-detail-resolver.ts
+++ src/app/resolver/position-detail-resolver.ts
@@ -74,30 +74,36 @@ export class PositionDetailResolver implements Resolve<PositionData>{
 
     private addLatestRateToLineChart(position: Position, rates: StockRate[]): void
     {
-        if (position?.balance) {
-            if (position.balance.lastRate?.date instanceof Date) {
-                if (position.balance.lastRate.date > rates[rates.length - 1].date) {
+        if (position?.balance && position.balance.lastRate) {
+            const lastRateFromBalance = position.balance.lastRate;
+            if (lastRateFromBalance.date > rates[rates.length - 1].date && !this.areRatesEqual(lastRateFromBalance, rates[rates.length - 1])) {
+                if (position) {
                     if (position.share?.marketplace?.currency === 'GBX') {
                         // island apes shit!
                         const ratesCopy = StockRateCreator.createNewStockRate();
-                        ratesCopy.rate = position.balance.lastRate.rate * 100;
-                        ratesCopy.high = position.balance.lastRate.high;
-                        ratesCopy.low = position.balance.lastRate.low;
+                        ratesCopy.rate = lastRateFromBalance.rate * 100;
+                        ratesCopy.high = lastRateFromBalance.high;
+                        ratesCopy.low = lastRateFromBalance.low;
                         rates.push(ratesCopy);
                     } else {
                         if (position.share?.name && position.share?.name?.indexOf('BRC') > -1) {
-                            if (position.balance.lastRate.low === 0) {
-                                position.balance.lastRate.low = position.balance.lastRate.rate / 10;
+                            if (lastRateFromBalance.low === 0) {
+                                lastRateFromBalance.low = lastRateFromBalance.rate / 10;
                             }
-                            if (position.balance.lastRate.high === 0) {
-                                position.balance.lastRate.high = position.balance.lastRate.rate / 10;
+                            if (lastRateFromBalance.high === 0) {
+                                lastRateFromBalance.high = lastRateFromBalance.rate / 10;
                             }
                         }
-                        rates.push(position.balance.lastRate);
+                        rates.push(lastRateFromBalance);
                     }
                 }
             }
         }
+    }
+
+    private areRatesEqual(rate1: StockRate, rate2: StockRate): boolean
+    {
+        return rate1.rate === rate2.rate && rate1.high === rate2.high;
     }
 
 }
