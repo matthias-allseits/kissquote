f037e916b0d8bfd89647badbdb93fc7fa747ce1b
Matthias Allseits <matthias.allseits@symbionet.io>
2025-07-14 12:59:46 +0200

    sf7-migration: bugfix

--- src/app/creators/position-creator.ts
+++ src/app/creators/position-creator.ts
@@ -67,7 +67,7 @@ export class PositionCreator {
                 apiArray.manualDividend ? apiArray.manualDividend : undefined,
                 apiArray.manualTargetPrice ? apiArray.manualTargetPrice : undefined,
                 apiArray.manualDrawdown ? apiArray.manualDrawdown : undefined,
-                apiArray.manualDividendDrop !== undefined && !isNaN(apiArray.manualDividendDrop) ? apiArray.manualDividendDrop : undefined,
+                apiArray.manualDividendDrop && !isNaN(apiArray.manualDividendDrop) ? apiArray.manualDividendDrop : undefined,
                 apiArray.manualDividendExDate ? new Date(apiArray.manualDividendExDate) : undefined,
                 apiArray.manualDividendPayDate ? new Date(apiArray.manualDividendPayDate) : undefined,
                 apiArray.manualDividendAmount ? apiArray.manualDividendAmount : undefined,
